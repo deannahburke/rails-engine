f03be1bc62fc83daf22d2879683d82088e28513a
Deannah Burke <deannah@bierstadtlager.com>
2022-07-14 18:04:28 -0600

    Add tests, modify controllers for search function

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::SearchController < ApplicationController
   def merchant_search
-
+    query_params = (params[:name])
+    if query_params.blank?
+      render status: 400
+    else
+      merchant = Merchant.search(query_params)
+      if merchant.nil?
+        render json: {data: {}}
+      else
+        render json: MerchantSerializer.new(merchant)
+      end
+    end
   end
 end
